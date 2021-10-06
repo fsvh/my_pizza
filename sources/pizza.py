ccc9acd0fab9230b130353dcddd5e41561a78965
fsvh <fsvh@me.com>
2021-10-06 08:39:07 +0200

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+        'my pizza': {
+        'ingredients': {'ham': 3, 'parmesan': 4, 'oregano': 3},
+        'price': 11,
     }
 }
